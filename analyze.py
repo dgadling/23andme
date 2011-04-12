889fc09da22564c07ce1da381360976037465c1c
Romain Komorn <rkomorn@fb.com>
2011-04-12 00:03:11 -0700

    Yes we can.

--- analyze.py
+++ analyze.py
@@ -16,20 +16,28 @@ for input_path in sys.argv[1:]:
             [line.split('\t')[3].strip() for line in input_fh
                 if line.startswith('#') == False ])
     print "Read in %d genotype components" % len(as_str)
-    print "  First occurence of 'GATTACA' @ %d" % as_str.find('GATTACA')
-    print "   Last occurence of 'GATTACA' @ %d" % as_str.rfind('GATTACA')
-    print " Total occurences of 'GATTACA' = %d" % as_str.count('GATTACA')
-    print " Kitty score (# of 'CAT's)     = %d" % as_str.count('CAT')
-    print " Who's it? (# of 'TAG's)       = %d" % as_str.count('TAG')
-    print " GATT compliance               = %d" % as_str.count('GATT')
+    print "  First occurence of 'GATTACA'  @ %d" % as_str.find('GATTACA')
+    print "   Last occurence of 'GATTACA'  @ %d" % as_str.rfind('GATTACA')
+    print " Total occurences of 'GATTACA'  = %d" % as_str.count('GATTACA')
+    print " Kitty score (# of 'CAT's)      = %d" % as_str.count('CAT')
+    print " Who's it? (# of 'TAG's)        = %d" % as_str.count('TAG')
+    print " GATT compliance                = %d" % as_str.count('GATT')
+    print " ATT customer                   = %d" % as_str.count('ATT')
+    print " Sassy one (# of 'TAT's)        = %d" % as_str.count('TAT')
+    print " Lil Monsters (# of 'GAGA's)    = %d" % as_str.count('GAGA')
+    print " Perverts (# of 'TATA's)        = %d" % as_str.count('TATA')
+    print " Gross ones (# of 'GAG's)       = %d" % as_str.count('GAG')
+    print " Original Gangsta (# of 'GAT's) = %d" % as_str.count('GAT')
     print
     g_count = sum(1 for c in as_str if c == 'G')
     a_count = sum(1 for c in as_str if c == 'A')
     t_count = sum(1 for c in as_str if c == 'T')
     c_count = sum(1 for c in as_str if c == 'C')
-    jonx = sum(1 for c in as_str if c not in ['G', 'A', 'T', 'C'])
+    d_count = sum(1 for c in as_str if c == '-')
+    jonx = sum(1 for c in as_str if c not in ['G', 'A', 'T', 'C', '-'])
     print "Found %d G's" % g_count
     print "Found %d A's" % a_count
     print "Found %d T's" % t_count
     print "Found %d C's" % c_count
+    print "Found %d -'s" % d_count
     print "Found %d something elses" % jonx
